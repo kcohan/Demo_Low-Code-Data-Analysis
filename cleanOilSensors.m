a0959da5f5905e7b6e16b15dc9ca87b9012bfc85
kcohan <150845259+kcohan@users.noreply.github.com>
2023-11-15 22:09:28 -0500

    Delete cleanOilSensors.m

--- cleanOilSensors.m
+++
@@ -1,5 +0,0 @@
-function FlightData = cleanOilSensors(FlightData)
-	% Fill outliers
-	FlightData = filloutliers(FlightData,"linear","movmedian",100,...
-	    "DataVariables",["OilPressure","OilTemperature"]);
-end
